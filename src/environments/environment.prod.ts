2a87fe725ec31461b442d585bcf240c3dd7a6ee4
Alejandro Sanchez <88404850+alejoRegxi@users.noreply.github.com>
2025-03-18 08:24:48 -0300

    Update API URL in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlAPI: 'https://api.mrcosmeticni.com/api/', 
+  urlAPI: 'https://api.mrprofesionalni.com/api/', 
   appVersion: "1.0",
   USERDATA_KEY_STORAGE:"MRCosmetic",
   CHECKOUT_KEY_STORAGE:"Checkout",
